df207d4c9b8a38a7ae42ec3b420a5faf4cbc143f
mxxikr <mxxikr@gmail.com>
2022-05-10 02:49:06 +0900

    3강-단어 입력 시 검사 후 결과 값 출력

--- JAVA/src/kr/ac/knou/mxxikr/ScannerDemo.java
+++ JAVA/src/kr/ac/knou/mxxikr/ScannerDemo.java
@@ -5,8 +5,10 @@ import java.util.Scanner;
 public class ScannerDemo {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in); // scanner 객체 생성
-		while (sc.hasNextInt()) { // 단어 입력 시 검사
+		if (sc.hasNextInt()) {
 			System.out.println(sc.nextInt());
+		} else {
+			System.out.println("It's not Integer");
 		}
 	}
 }
