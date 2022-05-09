11f8b9a940ad3e7fc3f789b320f959738d2f6aa4
mxxikr <mxxikr@gmail.com>
2022-05-10 00:56:06 +0900

    1강-Hello JAVA

---
+++ JAVA/src/kr/ac/knou/mxxikr/Hello.java
@@ -0,0 +1,7 @@
+package kr.ac.knou.mxxikr;
+
+public class Hello { // 클래스 정의 블록
+	public static void main(String args[]) { // 프로그램 실행시 main  함수 필요
+		System.out.println("Hello, Java Application!"); // 출력문
+	}
+} 
