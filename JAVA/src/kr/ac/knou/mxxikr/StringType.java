b687a670f586bc71031e25899dee635798d78a40
mxxikr <mxxikr@gmail.com>
2022-05-10 02:37:28 +0900

    3강-문자열

---
+++ JAVA/src/kr/ac/knou/mxxikr/StringType.java
@@ -0,0 +1,11 @@
+package kr.ac.knou.mxxikr;
+
+public class StringType {
+	public static void main(String[] args) {
+		String result = new String("Java");
+		
+		System.out.println("result=" + " " + result);
+		System.out.println('A'+0); //65
+		System.out.println("A" + 0); 
+	}
+}
