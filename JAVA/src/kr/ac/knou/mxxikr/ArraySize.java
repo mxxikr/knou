0409128b8db4432e8e2aa8f0ac4a351c5df387ff
mxxikr <mxxikr@gmail.com>
2022-05-10 02:34:02 +0900

    3강-배열

---
+++ JAVA/src/kr/ac/knou/mxxikr/ArraySize.java
@@ -0,0 +1,12 @@
+package kr.ac.knou.mxxikr;
+
+public class ArraySize {
+	public static void main(String[] args) {
+		int twoArray[][] = { { 0, 1 }, { 10, 11, 12 } }; // 선언과 동시에 초기화
+		for (int i = 0; i < twoArray.length; i++) {
+			for (int j = 0; j < twoArray[i].length; j++) {
+				System.out.println(twoArray[i][j]);
+			}
+		}
+	}
+}
---
+++ JAVA/src/kr/ac/knou/mxxikr/ArrayType.java
@@ -0,0 +1,15 @@
+package kr.ac.knou.mxxikr;
+
+public class ArrayType {
+	public static void main(String[] args) {
+		int[] a1 = {2, 3, 5, 7, 11};
+		int a2[] = new int[3];
+		
+		int b1[]; b1 = new int[10];
+		
+		int[ ][ ] c; int d[ ][ ]; int[ ] e[ ];
+		
+		int anArray[ ][ ] = {{1, 2, 3}, {4, 5, 6}};
+		int anArray4[ ][ ] = new int[3][2];
+	}
+}
