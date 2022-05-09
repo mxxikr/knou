3ac4a3343e090dbbd976f2d5a790427fe8cca21b
mxxikr <mxxikr@gmail.com>
2022-05-10 00:57:12 +0900

    2강-JAVA 데이터 타입

---
+++ JAVA/src/kr/ac/knou/mxxikr/DataType.java
@@ -0,0 +1,24 @@
+package kr.ac.knou.mxxikr;
+
+class Circle {
+	protected int radius = 1;
+}
+
+class Literal {
+	final static int nConst = 3; // 선언시 초기 값 지정
+}
+
+public class DataType {
+
+	public static void main(String[] args) {
+		Circle c = new Circle();
+		float f = (float) 5.5;
+
+		System.out.println(1 + 3);
+		System.out.println("a" + "bcd");
+		System.out.println(c.radius);
+		System.out.println(Literal.nConst);
+		System.out.println(f);
+	}
+
+}
---
+++ JAVA/src/kr/ac/knou/mxxikr/HelloApplication.java
@@ -0,0 +1,10 @@
+package kr.ac.knou.mxxikr;
+
+public class HelloApplication {
+	static String szMsg = "Hello, Java!";
+	
+	public static void main(String[] args) {
+		int nTest = 0;
+		System.out.println(szMsg);
+	}
+}
