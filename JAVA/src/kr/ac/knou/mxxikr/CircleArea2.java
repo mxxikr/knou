08f2572d1cfaa5746f5013be5546676c047078b2
mxxikr <mxxikr@gmail.com>
2022-05-12 00:48:26 +0900

    4강-static 필드와 객체의 사용

--- JAVA/src/kr/ac/knou/mxxikr/CircleArea2.java
+++ JAVA/src/kr/ac/knou/mxxikr/CircleArea2.java
@@ -2,14 +2,17 @@ package kr.ac.knou.mxxikr;
 
 public class CircleArea2 {
 	public static void main(String args[]) {
-		CircleCalc2 c = new CircleCalc2(5.0); // 생성자 호출
+		CircleCalc2 c1 = new CircleCalc2(5.0); // 생성자 호출
+		CircleCalc2 c2 = new CircleCalc2(4.0); // 생성자 호출
 		
-		System.out.println(c.r); // r에 접근제어자가 존재하지않아 같은 패키지의 다른 클래스에서 사용 가능
-		System.out.println(c.getArea());
+		System.out.println(c1.r); // r에 접근제어자가 존재하지않아 같은 패키지의 다른 클래스에서 사용 가능
+		System.out.println(c1.getArea());
+		System.out.println(c1.count);
 		
-		c.r = 5; // c의 r을 변경, 객체 사용
-		System.out.println(c.r);
-		System.out.println(c.getArea()); // c에게 getArea() 실행을 요청
+		c2.r = 6; // c의 r을 변경, 객체 사용
+		System.out.println(c2.r);
+		System.out.println(c2.getArea()); // c에게 getArea() 실행을 요청
+		System.out.println(c2.count);
 	}
 
 }
--- JAVA/src/kr/ac/knou/mxxikr/CircleCalc2.java
+++ JAVA/src/kr/ac/knou/mxxikr/CircleCalc2.java
@@ -1,7 +1,9 @@
 package kr.ac.knou.mxxikr;
 
 class CircleCalc2 { // 클래스 이름과 생성자 이름이 같음
+	static int count;
 	double r;
+	
 	public CircleCalc2(double a) { // 생성자 정의
 		r = a; // 반환형 존재하지않음
 	}
