44da91b052c61f53b1e2a9514e1e5d26997467db
mxxikr <mxxikr@gmail.com>
2022-05-12 01:15:59 +0900

    4강-static 필드 선언문 초기화와 초기화 블록 실행

--- JAVA/src/kr/ac/knou/mxxikr/IniTest.java
+++ JAVA/src/kr/ac/knou/mxxikr/IniTest.java
@@ -1,12 +1,17 @@
 package kr.ac.knou.mxxikr;
 
 class IniTest {
-	int nValue = 1; // 초기값 선언하지 않을경우 0으로 자동 초기화
-	{
-		nValue = 2; // 인스턴스 변수 초기값 변경
-	}
+	static int nValue = 1; // 초기값 선언하지 않을경우 0으로 자동 초기화
 
+	static {
+		nValue = 2;  // 클래스 변수 초기화 블록
+	}
+	
 	public IniTest() {
 		nValue = 3; // 초기값 변경
+	}
+	
+	public static void main(String[] args) {
+		System.out.print(nValue); // 두번 초기화된 2 출력
 	}
 }
