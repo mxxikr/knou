a2f2d3113f289fa988a6152da52a6d3211dca312
mxxikr <mxxikr@gmail.com>
2022-05-12 00:53:50 +0900

    4강-final 필드

--- JAVA/src/kr/ac/knou/mxxikr/CircleCalc2.java
+++ JAVA/src/kr/ac/knou/mxxikr/CircleCalc2.java
@@ -2,6 +2,7 @@ package kr.ac.knou.mxxikr;
 
 class CircleCalc2 { // 클래스 이름과 생성자 이름이 같음
 	static int count;
+	final static double PI = 3.141592; // 상수 데이터 선언
 	double r;
 	
 	public CircleCalc2(double a) { // 생성자 정의
@@ -10,6 +11,6 @@ class CircleCalc2 { // 클래스 이름과 생성자 이름이 같음
 	
 	public double getArea() {
 //		return r * r * 3.14;
-		return this.r * this.r * 3.14;
+		return this.r * this.r * PI;
 	}
 }
