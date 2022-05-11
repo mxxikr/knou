fdc6d345f4e4711b5bb7d6877be41e43c3d83dc2
mxxikr <mxxikr@gmail.com>
2022-05-12 01:30:53 +0900

    4강-생성자 실행해 초기값 변경

--- JAVA/src/kr/ac/knou/mxxikr/IniTest.java
+++ JAVA/src/kr/ac/knou/mxxikr/IniTest.java
@@ -1,17 +1,18 @@
 package kr.ac.knou.mxxikr;
 
 class IniTest {
-	static int nValue = 1; // 초기값 선언하지 않을경우 0으로 자동 초기화
+	int nValue = 1; // 초기값 선언하지 않을경우 0으로 자동 초기화
 
-	static {
-		nValue = 2;  // 클래스 변수 초기화 블록
+	{
+		nValue = 2; // 클래스 변수 초기화 블록
 	}
-	
+
 	public IniTest() {
 		nValue = 3; // 초기값 변경
 	}
-	
+
 	public static void main(String[] args) {
-		System.out.print(nValue); // 두번 초기화된 2 출력
+		IniTest n = new IniTest(); // 생성자 호출
+		System.out.println(n.nValue); // 생성자를 통해 초기화된 3 출력
 	}
 }
