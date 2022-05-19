15627c29e8cf7c61a7184de194a378eee9697582
mxxikr <mxxikr@gmail.com>
2022-05-19 21:25:14 +0900

    6강-제네릭 타입과 자료형 검사

---
+++ JAVA/src/kr/ac/knou/mxxikr/GenericsTest2.java
@@ -0,0 +1,14 @@
+package kr.ac.knou.mxxikr;
+
+public class GenericsTest2 {
+	public static void main(String args[ ]) {
+		Data2<String> data = new Data2<String>( );
+		Integer i = new Integer(20);
+//		data.set(i); //컴파일 오류
+		String s = new String("String s");
+		data.set(s);
+		String string = (String) data.get();
+		System.out.println(s);
+		System.out.println(string);
+	}
+}
