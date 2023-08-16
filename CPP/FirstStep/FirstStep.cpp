3cfc79917d8c972dbe61b97e4d39436cd0f4cc52
mxxikr <mxxikr@astorm.co.kr>
2023-08-16 23:09:00 +0900

    C++ 프로그래밍 1강 - 주석 처리

--- CPP/FirstStep/FirstStep.cpp
+++ CPP/FirstStep/FirstStep.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 
-int main()
-{
+int main() // 머리부
+{ // 몸체 블록
 	std::cout << "나의 첫번째 c++ 프로그램" << std::endl;
+	
+
+	/* a와 b의 값 중에서
+	 더 큰 값을 출력한다. */
+	int a = 10;
+	int b = 50;
+	if (a > b)
+		std::cout << a << std::endl;
+	else
+		std::cout << b << std::endl;
+
 	return 0;
 }
