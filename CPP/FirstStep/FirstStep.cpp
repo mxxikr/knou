2b3f7bc5b3259f0d0acfaaddadfbb960fa27864a
mxxikr <mxxikr@astorm.co.kr>
2023-08-16 23:12:36 +0900

    C++ 프로그래밍 1강 - 블록 처리 및 표준 입출력 스트림 사용

--- CPP/FirstStep/FirstStep.cpp
+++ CPP/FirstStep/FirstStep.cpp
@@ -2,17 +2,22 @@
 
 int main() // 머리부
 { // 몸체 블록
-	std::cout << "나의 첫번째 c++ 프로그램" << std::endl;
-	
+	std::cout << "나의 첫번째 c++ 프로그램" << std::endl; // 표준 출력 스트림
 
 	/* a와 b의 값 중에서
 	 더 큰 값을 출력한다. */
 	int a = 10;
 	int b = 50;
-	if (a > b)
-		std::cout << a << std::endl;
-	else
+	if (a > b) { 
+		std::cout << a << std::endl; 
+	}
+	else {
 		std::cout << b << std::endl;
+	}
+
+	std::cin >> a; 	// 표준 입력 스트림
+	int aSquare = a * a;
+	std::cout << "입력값 a * a = " << aSquare;
 
 	return 0;
 }
