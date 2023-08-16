fe8781cf2dc0cb69725c8831c84c984224118a26
mxxikr <mxxikr@astorm.co.kr>
2023-08-16 23:21:39 +0900

    C++ 프로그래밍 1강 - 표준 입력 스트림 활용

--- CPP/FirstStep/FirstStep.cpp
+++ CPP/FirstStep/FirstStep.cpp
@@ -2,7 +2,7 @@
 
 int main() // 머리부
 { // 몸체 블록
-	std::cout << "나의 첫번째 c++ 프로그램" << std::endl; // 표준 출력 스트림
+	std::cout << "나의 첫번째 c++ 프로그램" << std::endl; // 표준 출력 스트림으로 문장을 출력함
 
 	/* a와 b의 값 중에서
 	 더 큰 값을 출력한다. */
@@ -17,7 +17,24 @@ int main() // 머리부
 
 	std::cin >> a; 	// 표준 입력 스트림
 	int aSquare = a * a;
-	std::cout << "입력값 a * a = " << aSquare;
+	std::cout << "입력값 a * a = " << aSquare << std::endl;
+
+	/*
+		a가 b보다 작을 경우 
+		a와 b 값 변환한다. */
+
+	if (a < b) {
+		auto tmp = a;
+		a = b;
+		b = tmp;
+	}
+	std::cout << "a = " << a << std::endl;
+	std::cout << "b = " << b << std::endl;
+
+	int c;
+	char str[100];
+	std::cin >> c >> str;
+	std::cout << c << str;
 
 	return 0;
 }
