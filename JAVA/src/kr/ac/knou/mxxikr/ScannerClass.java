61e4158e52d0a4163ee966e27eeabb07195fc5de
mxxikr <mxxikr@gmail.com>
2024-01-31 04:06:16 +0900

    3강-Scanner 클래스 예제 수정

--- JAVA/src/kr/ac/knou/mxxikr/ScannerClass.java
+++ JAVA/src/kr/ac/knou/mxxikr/ScannerClass.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class ScannerClass {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in); // 인자로 표준 입력 이용해 객체 생성
-		String name = sc.next();
+		String name = sc.next(); // 단어를 읽어 String으로 반환
 		System.out.println(name);
 	}
 }
--- JAVA/src/kr/ac/knou/mxxikr/ScannerDemo.java
+++ JAVA/src/kr/ac/knou/mxxikr/ScannerDemo.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class ScannerDemo {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in); // scanner 객체 생성
-		if (sc.hasNextInt()) {
+		if (sc.hasNextInt()) { // int 값이 있을 경우 true 반환
 			System.out.println(sc.nextInt());
 		} else {
 			System.out.println("It's not Integer");
